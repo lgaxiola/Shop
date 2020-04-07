52dc36be4b46259a31c77dfbe8579d72381562e5
Lizeth Gaxiola <liz_amine@hotmail.com>
2020-04-06 17:05:29 -0700

    Products Controller

---
+++ Shop.Web4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop.Web4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
