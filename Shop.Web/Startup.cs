8ab06831d296c4a4212958e6f843c44c0fe94383
Lizeth Gaxiola <liz_amine@hotmail.com>
2020-04-07 17:19:31 -0700

    CLASE REPOSITORY - PRODUCTS FINALIZADA

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -38,6 +38,9 @@ namespace Shop.Web
             });
             
             services.AddTransient<SeedDb>();
+
+            services.AddScoped<IRepository, Repository>();
+
             
             services.Configure<CookiePolicyOptions>(options =>
             {
